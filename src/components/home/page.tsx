5f270ece8bbf9c0a874c1c20ac5506b18801c4d7
Sarath Madala <srthchndr125@gmail.com>
2023-09-09 19:35:29 -0500

    Setup Auth

---
+++ src/components/home/page.tsx
@@ -0,0 +1,17 @@
+"use client";
+import { SessionProvider, useSession } from 'next-auth/react';
+import Image from 'next/image'
+import { useEffect } from 'react';
+
+export default function Home() {
+  const { data: session, status } = useSession();
+  useEffect(() => {
+    console.log(session, status, 'session');
+  }, [session, status])
+  return (
+      <main className="flex min-h-screen flex-col items-center justify-between p-24">
+        {session && <div>Authenticated</div>} 
+        {!session && <div>Not Authenticated</div>}
+      </main>
+  )
+}
