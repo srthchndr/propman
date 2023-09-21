5464533fa790dfc816beacd0a0edda8ad917ed3f
srthchndr <30964943+srthchndr@users.noreply.github.com>
2023-09-20 19:43:27 -0500

    Added Redux store and RTK Query

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,9 +4,56 @@ import { SessionProvider, useSession } from 'next-auth/react';
 import Image from 'next/image'
 import React, { useEffect } from 'react';
 
+import { decrement, increment, reset } from "@/store/reducers/counterSlice";
+import { useAppDispatch, useAppSelector } from "@/store/hooks";
+import { useGetUsersQuery } from '@/store/services/userApi';
+
 export default function App() {
+  const count = useAppSelector((state) => state.counterReducer.value);
+  const dispatch = useAppDispatch();
+
+  const { isLoading, isFetching, data, error } = useGetUsersQuery(null);
+  
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div style={{ marginBottom: "4rem", textAlign: "center" }}>
+        <h4 style={{ marginBottom: 16 }}>{count}</h4>
+        <button onClick={() => dispatch(increment())}>increment</button>
+        <button
+          onClick={() => dispatch(decrement())}
+          style={{ marginInline: 16 }}
+        >
+          decrement
+        </button>
+        <button onClick={() => dispatch(reset())}>reset</button>
+      </div>
+      {error ? (
+        <p>Oh no, there was an error</p>
+      ) : isLoading || isFetching ? (
+        <p>Loading...</p>
+      ) : data ? (
+        <div
+          style={{
+            display: "grid",
+            gridTemplateColumns: "1fr 1fr 1fr 1fr",
+            gap: 20,
+          }}
+        >
+          {data.map((user) => (
+            <div
+              key={user.id}
+              style={{ border: "1px solid #ccc", textAlign: "center" }}
+            >
+              <img
+                src={`https://robohash.org/${user.id}?set=set2&size=180x180`}
+                alt={user.name}
+                style={{ height: 180, width: 180 }}
+              />
+              <h3>{user.name}</h3>
+            </div>
+          ))}
+        </div>
+      ) : null}
       <Home/>
     </main>
   )
